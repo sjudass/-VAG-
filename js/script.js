b50cf7e459ffe5f0040d386c9c1d15df755ceec4
sjudass <i.a.pichuev@gmail.com>
2022-05-18 00:27:26 +0300

    Правки по стилям

--- js/script.js
+++ js/script.js
@@ -78,7 +78,7 @@ function generateRatesHTMLBlock() {
         let cards = ``;
 
         reviews.forEach(function (review) {
-            cards += `<div class="carousel-card col-md-3">
+            cards += `<div class="carousel-card col-md-4">
                 <svg xmlns="http://www.w3.org/2000/svg" width="140" height="140" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
                     <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
                     <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
@@ -97,13 +97,7 @@ function generateRatesHTMLBlock() {
         </div>`);
     });
 
-    let carouselItems = document.getElementsByClassName('carousel-item');
-
-    for (let i = 0; i < carouselItems.length; i++) {
-        carouselItems[i].getElementsByClassName('carousel-card')[0].classList.add('offset-md-1');
-    }
-
-    carouselItems[0].classList.add('active');
+    document.getElementsByClassName('carousel-item')[0].classList.add('active');
 }
 
 function chunkArray(array, perChunk = 3) {
